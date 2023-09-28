21fb79ba494a4c7e0fc7ff9f509f32c89d0c9c05
Bismark Agyare <bismarkagyare54@gmail.com>
2023-09-28 12:20:18 +0000

    feat: Implement form with guess input field

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 import { sample } from '../../utils';
 import { WORDS } from '../../data';
@@ -8,8 +9,30 @@ const answer = sample(WORDS);
 // To make debugging easier, we'll log the solution in the console.
 console.info({ answer });
 
-function Game() {
-  return <>Put a game here!</>;
-}
+const Game = () => {
+  const [guess, setGuess] = useState('');
+
+  const handleSubmit = (e) => {
+    console.log('guess: ', guess);
+    e.preventDefault();
+    setGuess('');
+  };
+
+  return (
+    <div>
+      <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+        <label htmlFor="guess-input">Enter guess:</label>
+        <input
+          id="guess-input"
+          type="text"
+          value={guess}
+          onChange={(e) => setGuess(e.target.value.toUpperCase())}
+          pattern=".{5}"
+          maxLength={5}
+        />
+      </form>
+    </div>
+  );
+};
 
 export default Game;
