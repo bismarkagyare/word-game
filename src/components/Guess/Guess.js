e2644512e63f83ed3a85af1300e31789accb159f
Bismark Agyare <bismarkagyare54@gmail.com>
2023-10-04 21:24:22 +0000

    feat: Create a cell component, add style & status to guesses

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -1,13 +1,24 @@
 import React from 'react';
 import { range } from '../../utils';
+import { checkGuess } from '../../game-helpers';
 
-const Guess = ({ value }) => {
+const Cell = ({ letter, status }) => {
+  const className = status ? `cell ${status}` : 'cell';
+  return <span className={className}>{letter}</span>;
+};
+
+const Guess = ({ value, answer }) => {
+  const result = checkGuess(value, answer);
+  console.log({ result });
+
   return (
     <p className="guess">
       {range(5).map((num) => (
-        <span key={num} className="cell">
-          {value ? value[num] : undefined}
-        </span>
+        <Cell
+          key={num}
+          letter={result ? result[num].letter : undefined}
+          status={result ? result[num].status : undefined}
+        />
       ))}
     </p>
   );
